34ab812923a1244ed4d6a258cef487e649daa653
Daniel Sanabria <danielsanabriaalbert@gmail.com>
2020-03-21 11:50:50 +0100

    cambio grados

--- src/js/weather.js
+++ src/js/weather.js
@@ -1,32 +1,25 @@
-
 function success(pos) {
-
-
   const url = `/.netlify/functions/weatherapi?lat=${pos.coords.latitude}&lon=${pos.coords.longitude}&units=metric`;
 
   fetch(url)
-    .then((response) => {
+    .then(response => {
       return response.json();
     })
-    .then((data) => {
+    .then(data => {
+      document.querySelector("#city").textContent = data.name;
+      document.querySelector("#temp").textContent = data.main.temp + "C";
+      document.querySelector("#main").textContent = data.weather[0].main;
+      document.querySelector("#desc").textContent = data.weather[0].description;
 
-      document.querySelector('#city').textContent = data.name;
-      document.querySelector('#temp').textContent = data.main.temp + "°C";
-      document.querySelector('#main').textContent = data.weather[0].main;
-      document.querySelector('#desc').textContent = data.weather[0].description;
-
-      document.querySelector('#weather').classList.remove("hidden");
-      console.log('data :', data);
+      document.querySelector("#weather").classList.remove("hidden");
+      console.log("data :", data);
     });
-
 }
 
 function error(err) {
   console.warn(`ERROR(${err.code}): ${err.message}`);
 }
 
-if(navigator.geolocation) {
+if (navigator.geolocation) {
   navigator.geolocation.getCurrentPosition(success, error);
 }
-
-
